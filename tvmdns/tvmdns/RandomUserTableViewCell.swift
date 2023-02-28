34ec71ab8e5d24b76fecb48fd5db47c138c36fe6
Skyler Hope <99628593+sbhbob@users.noreply.github.com>
2023-02-28 10:19:57 -0700

    First commit

---
+++ tvmdns/tvmdns/RandomUserTableViewCell.swift
@@ -0,0 +1,27 @@
+//
+//  RandomUserTableViewCell.swift
+//  tvmdns
+//
+//  Created by Skyler Hope on 2/28/23.
+//
+
+import UIKit
+
+class RandomUserTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var userLabel: UILabel!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+    
+    
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
